1f5a8d88403b0d0a99d4d99dc534da11a6810c81
JedidiaRakotomalala <jedidiarakotomalala09@gmail.com>
2026-06-17 16:31:49 +0300

    commit problematique erreur: (modif  direct dans main)

--- fonction.php
+++ fonction.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 
     function dbconnect ()
@@ -107,3 +110,140 @@
         mysqli_free_result ( $req ) ;
         return $result ;
     }
+
+
+function get_all_departments_and_manager_s_name_and_numbre ()
+{
+    // Cette requête récupère le manager actuel (to_date > CURRENT_DATE)
+    // et compte le nombre total d'employés uniques (COUNT DISTINCT) dans le département
+    $sql = "SELECT 
+                d.dept_no, 
+                d.dept_name,
+                m_emp.first_name, 
+                m_emp.last_name,
+                COUNT(DISTINCT de.emp_no) AS nombre_employee
+            FROM departments d
+            JOIN dept_manager dm ON d.dept_no = dm.dept_no AND dm.to_date > CURRENT_DATE
+            JOIN employees m_emp ON dm.emp_no = m_emp.emp_no
+            JOIN dept_emp de ON d.dept_no = de.dept_no
+            GROUP BY 
+                d.dept_no, 
+                d.dept_name, 
+                m_emp.first_name, 
+                m_emp.last_name";
+
+    $req = mysqli_query(dbconnect(), $sql);
+    
+    // Sécurité au cas où la requête échouerait
+    if (!$req) {
+        die("Erreur SQL : " . mysqli_error(dbconnect()));
+    }
+
+    $res = array();
+    while ($donnee = mysqli_fetch_assoc($req)) 
+    {
+        $res[] = $donnee;
+    }
+    mysqli_free_result($req);
+    return $res;
+}
+
+
+function get_n_employee ()
+{
+        $sql = " SELECT count(employees.emp_no) as ne FROM employees " ;
+        echo $sql ;
+        $req = mysqli_query(dbconnect(), $sql ) ;
+        $result = array () ;
+        while ( $line = mysqli_fetch_assoc ( $req ) ) 
+        {
+            $result[] = $line ;
+        }
+        mysqli_free_result ( $req ) ;
+        return $result ;
+}
+
+function get_n_employee_m ()
+{
+        $sql = " SELECT count(employees.emp_no) as ne FROM employees WHERE gender = 'M' " ;
+        echo $sql ;
+        $req = mysqli_query(dbconnect(), $sql ) ;
+        $result = array () ;
+        while ( $line = mysqli_fetch_assoc ( $req ) ) 
+        {
+            $result[] = $line ;
+        }
+        mysqli_free_result ( $req ) ;
+        return $result ;
+}
+
+function get_n_employee_f ()
+{
+        $sql = " SELECT count(employees.emp_no) as ne FROM employees WHERE gender = 'F' " ;
+        echo $sql ;
+        $req = mysqli_query(dbconnect(), $sql ) ;
+        $result = array () ;
+        while ( $line = mysqli_fetch_assoc ( $req ) ) 
+        {
+            $result[] = $line ;
+        }
+        mysqli_free_result ( $req ) ;
+        return $result ;
+}
+
+function get_salaire_moyenn_pour_chaque_emploi ()
+{
+        $sql = " SELECT 
+    titles.title AS emploi, 
+    ROUND(AVG(salaries.salary), 2) AS salaire_moyen
+FROM titles
+JOIN salaries ON titles.emp_no = salaries.emp_no
+GROUP BY 
+    titles.title; " ;
+        echo $sql ;
+        $req = mysqli_query(dbconnect(), $sql ) ;
+        $result = array () ;
+        while ( $line = mysqli_fetch_assoc ( $req ) ) 
+        {
+            $result[] = $line ;
+        }
+        mysqli_free_result ( $req ) ;
+        return $result ;
+}
+
+function determiner_l_emploi_le_plus_long ( $id_employe )
+{
+    $sql = "SELECT 
+                title AS emploi,
+                DATEDIFF(
+                    CASE WHEN to_date > CURRENT_DATE THEN CURRENT_DATE ELSE to_date END, 
+                    from_date
+                ) AS duree_jours
+            FROM titles
+            WHERE emp_no = %d
+            ORDER BY duree_jours DESC
+            LIMIT 1";
+            
+    $sql = sprintf($sql, $id_employe);
+    
+    $req = mysqli_query(dbconnect(), $sql);
+    
+    if (!$req) {
+        die("Erreur SQL : " . mysqli_error(dbconnect()));
+    }
+    
+    $result = array();
+    while ($line = mysqli_fetch_assoc($req)) 
+    {
+        $result[] = $line;
+    }
+    mysqli_free_result($req);
+    
+    return $result; // Retourne un tableau contenant l'emploi le plus long et sa durée
+}
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -16,6 +16,82 @@
 
     <br>
     <br>
+    <br>
+    <br>
+
+
+
+
+
+
+    <h3>Les departements sont : </h3>
+    <br>
+    <h3>Les departements avec les noms des managers en cour : </h3>
+    <br>
+    <table border="1px solide" >
+        <tr>
+            <th>Le numero des departements </th>
+            <th>Le nom des departements </th>
+            <th>Le first name des managers </th>
+            <th>Le last name des managers </th>
+        </tr>
+        <?php 
+            $taille = count ( $tout_les_departements_et_manager ) ;
+            for ( $i = 0 ; $i < $taille ; $i ++ ) 
+            { 
+        ?>
+            <tr>
+                <td><?php echo $tout_les_departements_et_manager[$i]["dept_no"] ; ?></td>
+                <td><?php echo $tout_les_departements_et_manager[$i]["dept_name"] ; ?></td>
+                <td><?php echo $tout_les_departements_et_manager[$i]["first_name"] ; ?></td>
+                <td><?php echo $tout_les_departements_et_manager[$i]["last_name"] ; ?></td>
+            </tr>
+        <?php
+            } 
+        ?>
+    </table>
+
+    <br>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <br>
+    <br>
+
+
+    <h3>Les departements avec les noms des managers en cour : </h3>
+    <br>
+    <table border="1px solide" >
+        <tr>
+            <th>Le numero des departements </th>
+            <th>Le nom des departements </th>
+            <th>Le first name des managers </th>
+            <th>Le last name des managers </th>
+        </tr>
+        <?php 
+            $taille = count ( $tout_les_departements_et_manager ) ;
+            for ( $i = 0 ; $i < $taille ; $i ++ ) 
+            { 
+        ?>
+            <tr>
+                <td><?php echo $tout_les_departements_et_manager[$i]["dept_no"] ; ?></td>
+                <td><?php echo $tout_les_departements_et_manager[$i]["dept_name"] ; ?></td>
+                <td><?php echo $tout_les_departements_et_manager[$i]["first_name"] ; ?></td>
+                <td><?php echo $tout_les_departements_et_manager[$i]["last_name"] ; ?></td>
+            </tr>
+        <?php
+            } 
+        ?>
+    </table>
+
+    <br>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <br>
+    <br>
 
     <h3>Les departements avec les noms des managers en cour avec les liens : </h3>
     <br>
